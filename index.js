a94369883287d8b03e55981c6e7315a9f98cebdd
Emanuele Fumagalli <emanuelef@gmail.com>
2018-07-04 15:04:43 +0100

    refactor to use common repo and new entrypoint

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict';
 require('dotenv').config();
 const Boom = require('boom');
-const {startQueryingPromise, startQueryingFlightsPromise, startQueryingAllFilteredPromise} = require('./mysql');
+const {
+  startQueryingPromise,
+  startQueryingFlightsPromise,
+  startQueryingAllFilteredPromise
+} = require('air-commons').mysql;
 const Hapi = require('hapi');
-const server = Hapi.server({port: process.env.PORT});
+const server = Hapi.server({
+  port: process.env.PORT
+});
 
 const init = async () => {
 
@@ -98,6 +104,42 @@ server.route({
 
     try {
       let items = await startQueryingAllFilteredPromise(params.from, params.to, Number(params.lat), Number(params.lon), Number(params.max));
+      console.log('DONE ', items.length);
+      let response = {
+        fromDate: new Date(params.from * 1000),
+        toDate: new Date(params.to * 1000),
+        totalItems: items.length
+      };
+
+      response.items = items;
+
+      return response;
+    } catch (e) {
+      console.log(e);
+      throw Boom.teapot(e.message);
+    }
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/allFlightsInBox',
+  handler: async (request, h) => {
+    const params = request.query;
+    console.log(params);
+
+    try {
+      let items = await startQueryingAllFilteredPromise(params.from, params.to, Number(params.lat), Number(params.lon), Number(params.max));
+
+      let flightsByIcao = {};
+
+      for (let item of items) {
+        flightsByIcao[item.icao] = flightsByIcao[item.icao] || 0;
+        flightsByIcao[item.icao] = flightsByIcao[item.icao] + 1;
+      }
+
+      console.log(flightsByIcao);
+
       console.log('DONE ', items.length);
       let response = {
         fromDate: new Date(params.from * 1000),
